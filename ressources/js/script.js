82c82246b7bfbb45bb6bd3bcd33c59d95956d34f
Stepan <pl77sbr@eduvaud.ch>
2026-01-23 10:55:08 +0100

    feat(dessiner les différentes formes sur la grille): Terminé
    [55] [Done]

--- ressources/js/script.js
+++ ressources/js/script.js
@@ -3,18 +3,18 @@ const largeur = 10;
 const hauteur = 20;
 
 let positionX = 4;
-let positionY = 0;
+let positionY = 1;
 
 const numForm = Math.floor(Math.random() * 5);
 const numRotation = 0;
 
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < largeur * hauteur; i++) {
   const cell = document.createElement("div");
   cell.classList.add("cell");
   grid.appendChild(cell);
 }
 
-var form = new Array();
+let form = new Array();
 //Carré
 form[0] = [
   [
@@ -25,10 +25,10 @@ form[0] = [
 //Ligne
 form[1] = [
   [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
     [1, 1, 1, 1],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
   ],
   [
     [1, 0, 0, 0],
@@ -96,10 +96,33 @@ form[4] = [
   ],
 ];
 
+var couleur;
+
 const cells = document.querySelectorAll(".cell");
 
+/// Dessiner la forme
 function draw() {
-  for (let index = 0; index < form[numForm]; index++) {
-    const element = array[index];
+  for (
+    let index = 0;
+    index < form[numForm][numRotation].length;
+    index++ //Lignes
+  ) {
+    for (
+      let jindex = 0;
+      jindex < form[numForm][numRotation][index].length;
+      jindex++ //Colonnes
+    ) {
+      if (form[numForm][numRotation][index][jindex] === 1) {
+        const cellIndex = (positionY + index) * largeur + (positionX + jindex);
+        cells[cellIndex].style.backgroundColor = "red";
+      }
+    }
   }
 }
+function clearGrid() {
+  cells.forEach((cell) => {
+    cell.style.backgroundColor = "#111";
+  });
+}
+clearGrid();
+draw();
